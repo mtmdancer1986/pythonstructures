937134cc9b9d54b1fe5cf1b4aa6052cc1980e8a2
McKay Montz <38731737+mtmdancer1986@users.noreply.github.com>
2021-09-21 11:21:56 -0400

    Add files via upload

---
+++ python-ds-practice/04_number_compare/number_compare.py
@@ -0,0 +1,18 @@
+def number_compare(a, b):
+    """Report on whether a>b, b>a, or b==a
+    
+        >>> number_compare(1, 1)
+        'Numbers are equal'
+        
+        >>> number_compare(-1, 1)
+        'Second is greater'
+        
+        >>> number_compare(1, -2)
+        'First is greater'
+    """
+    if a > b:
+        return f"{a} is greater than {b}"
+    elif b > a:
+        return f"{b} is greater than {a}"
+    else:
+        return "Numers are equal"
